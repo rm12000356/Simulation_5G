b7bb156fc072dd97c9fd081a3e41f7034f6a6dd2
Rami Nouehed <127147493+rm12000356@users.noreply.github.com>
2025-10-30 11:46:12 +0200

    Refactor binary decoding logic
    Refactor decode_from_binary to handle chunks of bits

--- ASCII.py
+++ ASCII.py
@@ -2,6 +2,12 @@ def encode_to_binary(word):
 
     return [bin(ord(char))[2:].zfill(8) for char in word]
 
-def decode_from_binary(binary_list):
+def decode_from_binary(bits):
+    binary_strings_dec = []
+    for i in range(0, len(bits), 8):
+        chunk = bits[i:i+8]
+        binary_str = ''.join(str(bit) for bit in chunk)
+        binary_strings_dec.append(binary_str)
 
-    return ''.join(chr(int(b, 2)) for b in binary_list)
+    return ''.join(chr(int(b, 2)) for b in binary_strings_dec)
+
